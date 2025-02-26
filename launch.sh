3966e87e520764cffa974e74a4b57a4ccfa4405f
therealbungus <trecko1234@gmail.com>
2025-02-25 23:09:18 -0600

    add server launcher script

--- launch.sh
+++ launch.sh
@@ -12,8 +12,8 @@ else
 fi
 
 if ! [[ -f 'Monifactory-Beta.0.11.5-server.zip' ]]; then
-	rm -fr config kubejs libraries mods Server*.zip forge*.jar
-	curl -Lo 'Monifactory-Beta.0.11.5-server.zip' 'https://github.com/ThePansmith/Monifactory/releases/download/0.11.5/Monifactory-Beta.0.11.5-server.zip' && unzip -u -o 'Monifactory-Beta.0.11.5-server.zip' -d /data
+	rm -fr config kubejs libraries mods Monifactory*.zip forge*.jar
+	curl -Lo 'Monifactory-Beta.0.11.5-server.zip' 'https://github.com/ThePansmith/Monifactory/releases/download/0.11.5/Monifactory-Beta.0.11.5-server.zip' && ln -s . overrides && unzip -u -o 'Monifactory-Beta.0.11.5-server.zip' 'overrides/*' -d /data && rm overrides
 	# java -jar $(ls forge-*-installer.jar) --installServer
 fi
 
@@ -34,5 +34,7 @@ sed -i 's/server-port.*/server-port=25565/g' server.properties
 
 sed -i "s/-Xms4G -Xmx4G/$JVM_OPTS/" variables.txt
 
+curl -Lo '/data/start.sh' 'https://github.com/therealbungus/docker-monifactory/blob/master/start.sh'
+curl -Lo '/data/variables.txt' 'https://github.com/therealbungus/docker-monifactory/blob/master/variables.txt'
 chmod +x /data/start.sh
 /data/start.sh
